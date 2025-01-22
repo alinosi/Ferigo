cb3b401d8cf2c0772571ff03e6f5a2bdc566f66c
nobynobygon <146401086+alinosi@users.noreply.github.com>
2025-01-22 19:21:17 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,5 +5,5 @@
     //db
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'S@suke12');
-    define('DB_NAME', 'mvc_mahasiswa');
+    define('DB_PASS', ''); 
+    define('DB_NAME', ''); 
